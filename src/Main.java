0968db3087dbd36e298931c921bd4d95a1e63ed8
Evgeniy <varnavskiyev@gmail.com>
2022-11-06 15:13:45 +0300

    move folder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Бонусные мили: " + miles);
+
     }
 }
